7c843dcc259ac4980ae1b95bc514632572861846
cyla00 <ikhayam000@protonmail.com>
2020-07-06 15:38:17 +0200

    icons

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PortfolioIK</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <!-- SEZIONE CONTATTI -->
+    <div id="div_contatti">
+
+      <div class="contenitore_contatti">
+        <div class="alignamento_icone">
+          <div class="contatto">
+            <img class="icone_contatti" src="./img/linkedin.png" alt="">
+          </div>
+          <div class="titolo_icona_scomparsa">
+            <a href="https://www.linkedin.com/in/ismail-khayam/" class="titolo_icona_contatto" style="text-decoration: none;">LinekdIn</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="contenitore_contatti">
+        <div id="icona_centrale" class="alignamento_icone">
+          <div class="contatto">
+            <img class="icone_contatti" src="./img/github.png" alt="">
+          </div>
+          <div class="titolo_icona_scomparsa">
+            <a href="https://github.com/cyla00" class="titolo_icona_contatto" style="text-decoration: none;">GitHub</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="contenitore_contatti">
+        <div class="alignamento_icone">
+          <div class="contatto">
+            <img class="icone_contatti" src="./img/email.png" alt="">
+          </div>
+          <div class="titolo_icona_scomparsa">
+            <a href="https://github.com/cyla00" class="titolo_icona_contatto" style="text-decoration: none;">Mail</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </body>
+</html>
