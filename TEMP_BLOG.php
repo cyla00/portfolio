6bc050fa360e0e0c2a7f2a6670661ae220e99b04
cyla00 <ikhayam000@protonmail.com>
2020-07-07 12:19:10 +0200

    blog temp

---
+++ TEMP_BLOG.php
@@ -0,0 +1,40 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>TEMPLATE_Blog</title>
+  </head>
+  <body>
+    <div id="blog">
+      <?php require("settore_icone.php")?>
+
+      <div class="sfondo_blog">
+        <h1></h1>
+
+        <div class="blog_img1">
+          <img src="" alt="">
+        </div>
+
+        <div class="blog_into">
+          <h3></h3>
+          <p></p>
+        </div>
+
+        <div class="blog_tecnologie">
+          <img src="" alt="">
+          <img src="" alt="">
+          <img src="" alt="">
+          <img src="" alt="">
+        </div>
+
+      </div>
+
+      <?php require("settore_nome.php") ?>
+
+    </div>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -21,19 +21,9 @@
     </div>
 
     <div id="sezione_centrale">
-      <div class="container_bottone">
-        <div class="btn">
-          <button class="bottone" id="bottone_dietro" type="dietro" name="button"><img class="immagine_bottone" src="./img/left.png" alt="bottoneDietro"></button>
-        </div>
-      </div>
-      <div class="immagine_laptop">
-        <img id="laptop" src="./img/laptop.png" alt="laptop">
-      </div>
-      <div class="container_bottone">
-        <div class="btn">
-          <button class="bottone" id="bottone_avanti" type="dietro" name="button"><img class="immagine_bottone" src="./img/right.png" alt="bottoneAvanti"></button>
-        </div>
-      </div>
+
+      <?php require("settore_laptop.php") ?>
+
     </div>
 
 
