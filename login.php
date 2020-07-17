00b3f063340f8c9e3bf609aafe0d8db66784d456
cyla00 <ikhayam000@protonmail.com>
2020-07-17 17:05:12 +0200

    loginbtn popup

--- login.php
+++ login.php
@@ -10,7 +10,7 @@
     <?php
     require("settore_header.php");
      ?>
-     
+
     <?php
     require('connex.php');
 
@@ -18,20 +18,28 @@
 
     }
      ?>
-    <header>
-      <form class="" action="login.php" method="post">
-        <p>
-          <label for="user">Username</label>
-          <input  id="user" type="text" name="user" value="">
-        </p>
-        <p>
-          <label for="pass">Password</label>
-          <input  id="pass" type="text" name="pass" value="">
-        </p>
-        <p>
-          <input type="submit" name="login" value="Login">
-        </p>
-      </form>
-    </header>
+
+    <div id="contenitore_login">
+      <div class="vuoto"></div>
+
+      <div>
+        <form id="form_login" action="login.php" method="post">
+          <div class="cont_log">
+            <label for="user">Username</label>
+            <input class="inpt" type="text" name="user" value="">
+          </div>
+          <div class="cont_log">
+            <label for="pass">Password</label>
+            <input  class="inpt" type="text" name="pass" value="" oninput="">
+          </div>
+          <div class="cont_log">
+            <input id="submit_log" type="submit" name="login" value="Login">
+          </div>
+        </form>
+      </div>
+
+      <div class="vuoto"></div>
+    </div>
+    <script type="text/javascript" src="script.js"></script>
   </body>
 </html>
---
+++ script.js
@@ -0,0 +1,5 @@
+
+function loginpop(){
+    const loginUser = document.getElementByName("pass");
+    loginButton.style.display = "flex";
+}
